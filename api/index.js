a076d649e0a4c28a6b59a6297f327f7f56299291
divyajyotiuk <divyajyoti.u@gmail.com>
2020-08-29 18:32:58 +0530

    req res

--- api/index.js
+++ api/index.js
@@ -29,6 +29,8 @@ const name  = 'Divyajyoti'
 ;
 
 module.exports = (req, res) => {
+    const { name = 'World' } = req.query
+    res.status(200).send(`Hello ${name}!`);
     twitterClient.get('account/verify_credentials')
              .then((res) => {
                 const followerCount = res.followers_count
